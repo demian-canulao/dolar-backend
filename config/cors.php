53951582790d95b027fc6eb14932ff134a146b89
Demian <demian.canulao@gmail.com>
2025-11-21 02:11:11 -0300

    Se agrega url de vercel al cors

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,8 @@ return [
 
     'allowed_origins' => [
         'http://localhost:5173',
-        'http://127.0.0.1:5173'
+        'http://127.0.0.1:5173',
+        'https://dolar-frontend-4od6.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
